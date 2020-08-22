2e928aed3bff4ca6f67d01dae9cff5f05ad1d50f
fedemozzon <mozzonfederico@gmail.com>
2020-08-22 15:11:30 -0300

    Agrego el boton que mostrará los resultados

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,2 +1,7 @@
 document.querySelector("body") .style["background-color"] = "yellow";
+element = document.getElementById('hdtb-msb')
+boton = document.getElementById('hdtb-tls').cloneNode()
+boton.textContent = 'Extension'
+element.appendChild(boton)
 console.log('es amarillooo')
+console.log(boton.textContent)
