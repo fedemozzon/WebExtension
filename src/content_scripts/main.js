2038acb8991d1e704f1f6dbd2ec370335a724221
fedemozzon <mozzonfederico@gmail.com>
2020-08-25 13:34:03 -0300

    Agrego para obtener la palabra buscada y hago refactoring

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,7 +1,18 @@
-document.querySelector("body") .style["background-color"] = "yellow";
-element = document.getElementById('hdtb-msb')
-boton = document.getElementById('hdtb-tls').cloneNode()
-boton.textContent = 'Extension'
-element.appendChild(boton)
-console.log('es amarillooo')
-console.log(boton.textContent)
+insertButton()
+console.log(findWordSearched())
+//Inserta un boton en la barra de Google
+function insertButton(){
+    element = document.getElementById('hdtb-msb')
+    element.appendChild(copyButton())
+}
+//Copia un boton de la barra de Google para que sea con el mismo estilo
+function copyButton(){
+    button = document.getElementById('hdtb-tls').cloneNode()
+    button.textContent = 'Extensión'
+    return button
+}
+//Toma del buscador la palabra buscada
+function findWordSearched(){
+    busqueda = document.getElementsByName('q')
+    return busqueda[0].value
+}
