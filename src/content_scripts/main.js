a14d655edbe639e6114ac965468f7c7ec481e799
fedemozzon <mozzonfederico@gmail.com>
2020-08-27 15:55:29 -0300

    Abre y cierra popup

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -8,7 +8,7 @@ function insertButton(){
 //Copia un boton de la barra de Google para que sea con el mismo estilo
 function copyButton(){
     button = document.getElementById('hdtb-tls').cloneNode()
-    button.onclick = () => {makeAPopup()}
+    button.onclick = () => {showAPopup()}
     button.textContent = 'Mashup Results'
     return button
 }
@@ -18,11 +18,28 @@ function findWordSearched(){
     return busqueda[0].value
 }
 
+function showAPopup(){
+    if(document.getElementsByClassName('cF4V5c').length == 3){
+        if (document.getElementsByClassName('cF4V5c')[2].style.display == 'none'){
+            openPopup(document.getElementsByClassName('cF4V5c')[2])
+        }
+        else closePopup(document.getElementsByClassName('cF4V5c')[2])
+    }
+    else makeAPopup()
+}
+function closePopup(popup){
+    console.log(popup)
+    popup.style.display = 'none'
+}
+function openPopup(popup){
+    popup.style.display = 'initial'
+}
 function makeAPopup(){
     popup = (document.getElementsByClassName('cF4V5c')[1]).cloneNode(true)
-    element = document.getElementById('hdtb-msb')
-    popup.style.display = 'block'
+    element = document.getElementById('lb')
+    popup.style.left = '864.898px'
+    popup.style.top = '1119.5px'
+    popup.role = 'menu'
+    popup.tabIndex = '-1'
     element.appendChild(popup)
-    console.log(popup)
-    return element
 }
