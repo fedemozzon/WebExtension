8a0aca7a283c1a503c8e973a841a25a1fd4e72b1
fedemozzon <mozzonfederico@gmail.com>
2020-08-27 14:50:30 -0300

    Ahora cuando toca en Mashup results, despliega un cartel

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -8,12 +8,21 @@ function insertButton(){
 //Copia un boton de la barra de Google para que sea con el mismo estilo
 function copyButton(){
     button = document.getElementById('hdtb-tls').cloneNode()
-    button.onclick = () => alert('anda')
-    button.textContent = 'Extensión'
+    button.onclick = () => {makeAPopup()}
+    button.textContent = 'Mashup Results'
     return button
 }
 //Toma del buscador la palabra buscada
 function findWordSearched(){
     busqueda = document.getElementsByName('q')
     return busqueda[0].value
+}
+
+function makeAPopup(){
+    popup = (document.getElementsByClassName('cF4V5c')[1]).cloneNode(true)
+    element = document.getElementById('hdtb-msb')
+    popup.style.display = 'block'
+    element.appendChild(popup)
+    console.log(popup)
+    return element
 }
