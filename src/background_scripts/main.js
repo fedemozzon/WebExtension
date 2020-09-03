3764fd1954149de9511ef41f3bd75f3b2bbd4d89
fedemozzon <mozzonfederico@gmail.com>
2020-09-03 12:43:29 -0300

    Agrego como hacer un pedido HTTP a duck duck

--- src/background_scripts/main.js
+++ src/background_scripts/main.js
@@ -2,7 +2,11 @@ console.log('aber')
 //El parametro _ es porque en segundo lugar espera otro parametro, si no está, no anda
 chrome.runtime.onMessage.addListener(function (message,_,sendResponse) {
 console.log(message)
-console.log(sendResponse)
 sendResponse({response:'olaaaaaaa'})
+makeHTTPRequest()
 return true
 })
+
+function makeHTTPRequest(){
+    fetch('https://api.duckduckgo.com/?q=DuckDuckGo&format=json&pretty=1').then((response)=> console.log(response.json()))
+}
