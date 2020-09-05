317254c071acf9bf559f7a1c643db1619626c5ff
fedemozzon <mozzonfederico@gmail.com>
2020-09-05 12:30:50 -0300

    Consigo la información de bing

--- src/background_scripts/searcherBing.js
+++ src/background_scripts/searcherBing.js
@@ -1,5 +1,14 @@
 class SearcherBing extends Searcher{
-    
+    urlApi = "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?q="
+    key1 = '11b66270ed184719ad758b027e84c9e2'
+    key2 = 'b84b2a896e8246cc9652d1d7b10bbcb2'
+    customConfigurationID = '&customconfig=f275cdf4-9523-4cc7-988e-8d44c7eb0332'
+
+    searchOnTheWeb(word){
+        fetch(this.urlApi+word+this.customConfigurationID,{headers:{
+            'Ocp-Apim-Subscription-Key' : this.key1
+        }}).then((response) => {console.log(response.json()) })
+    }
 
 
 }
