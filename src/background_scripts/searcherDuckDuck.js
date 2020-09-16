2d4874fa71d898811a137c2da9753937bfdc1938
fedemozzon <mozzonfederico@gmail.com>
2020-09-16 13:54:48 -0300

    Agrego una reducción del arreglo que devuelve duckduck

--- src/background_scripts/searcherDuckDuck.js
+++ src/background_scripts/searcherDuckDuck.js
@@ -2,4 +2,10 @@ class SearcherDuckDuck extends Searcher{
     urlApi = "https://duckduckgo.com/html/?q="
     format = "" 
 
+    //En este caso reduzco el arreglo porque me da los resultados de varias páginas
+    // En general no superan los 11 para la primer página
+    htmlToResultList(html){
+        return Array.from(html.getElementsByClassName('links_main links_deep result__body')).slice(0,11)
+    }
+
 }
